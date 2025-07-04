c49c6be04fb4c49b84b67aa63b86b4782ea086cf
zhongyuwen <934816104@qq.com>
2025-07-04 18:30:14 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    host: '::', // 同时支持 IPv4 和 IPv6
+    port: 8421,
+    allowedHosts: 'all', // 允许所有 Host 请求
     proxy: {
       '/api': {
         target: process.env.VUE_APP_API_BASE_URL, // 使用环境变量
